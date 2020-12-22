09a256d2c82794f74f502376b4a4131e4cdf63e2
Rusewww <Rusewww1@gmain.com>
2020-12-22 12:42:39 -0500

    Skeleton

---
+++ src/lib.c
@@ -0,0 +1,23 @@
+#include "lib.h"
+
+/**
+* @file lib.c
+* @brief Реалізвція функцій для файлу main.c.
+* @author КІТ-120а.
+* @date 22-dec-2020
+* @version 1.0
+*/
+
+/**
+ * @param  - ;
+ * @param  - ;
+ * @param  - ;
+ * @param  - ;
+ * @param  - ;
+ * @param  - ;
+ */
+
+/**
+Функція : .
+*/
+
---
+++ src/lib.h
@@ -0,0 +1,12 @@
+/**
+* @file lib.h
+* @brief Попереднє задання функцій.
+* @author КІТ-120а.
+* @date 22-dec-2020
+* @version 1.0
+*/
+
+#ifndef RAID_FROM_120A_LIB_H
+#define RAID_FROM_120A_LIB_H
+
+#endif //RAID_FROM_120A_LIB_H
